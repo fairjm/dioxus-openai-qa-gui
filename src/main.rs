8d9761e95cbbca6ff0a12afb24b51c865cfda41a
yukin <fairjmflyer@gmail.com>
2023-03-16 00:49:14 +0800

    use icon lib. change layout

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use chrono::{DateTime, Utc};
 use dioxus::prelude::*;
 use dioxus_desktop::tao::window::Icon;
 use dioxus_desktop::{Config, WindowBuilder};
+use dioxus_free_icons::Icon;
+use dioxus_free_icons::icons::bs_icons::{BsGear, BsArrowDownShort, BsArrowUpShort};
+use dioxus_free_icons::icons::fa_brands_icons::FaGithub;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
@@ -66,11 +69,7 @@ fn app(cx: Scope) -> Element {
                             target: "_blank",
                             href: "https://github.com/fairjm/dioxus-openai-qa-gui",
                             span { class: "icon is-small",
-                                svg {
-                                    "xmlns": "http://www.w3.org/2000/svg",
-                                    "viewBox": "0 0 24 24",
-                                    path { "d": "M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" }
-                                }
+                                Icon { width: 24, height: 24, fill: "#6e7781", icon: FaGithub }
                             }
                             span { "GitHub" }
                         }
@@ -89,9 +88,7 @@ fn app(cx: Scope) -> Element {
                     }
                 },
                 span { class: "icon has-text-light",
-                    svg { "xmlns": "http://www.w3.org/2000/svg", "viewBox": "0 0 24 24",
-                        path { "d": "M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" }
-                    }
+                    Icon { width: 24, height: 24, fill: "#6e7781", icon: BsGear }
                 }
                 span { "设置" }
             }
@@ -129,93 +126,110 @@ fn app(cx: Scope) -> Element {
                     }
                 }
             }
+
             div { class: "columns",
-                div { class: "column",
+                div { class: "column pb-1",
                     nav { class: "level mb-1",
                         div { class: "level-left",
                             div { class: "level-item", p { class: "has-text-grey-light", "系统prompt" } }
-                        }
-                    }
-                    div { class: "block mb-1",
-                        div { class: "dropdown {system_prompt_dropdown}",
-                            div { class: "dropdown-trigger",
-                                button {
-                                    class: "button",
-                                    "aria-haspopup": true,
-                                    "aria-controls": "dropdown-menu",
-                                    onclick: move |_| {
-                                        if system_prompt_dropdown.current().is_empty() {
-                                            system_prompt_dropdown.set("is-active");
-                                        } else {
-                                            system_prompt_dropdown.set("");
+                            div { class: "level-item",
+                                div { class: "dropdown {system_prompt_dropdown}",
+                                    div { class: "dropdown-trigger",
+                                        button {
+                                            class: "button is-small",
+                                            "aria-haspopup": true,
+                                            "aria-controls": "dropdown-menu",
+                                            onclick: move |_| {
+                                                if system_prompt_dropdown.current().is_empty() {
+                                                    system_prompt_dropdown.set("is-active");
+                                                } else {
+                                                    system_prompt_dropdown.set("");
+                                                }
+                                            },
+                                            span { "prompt列表" }
+                                            span { class: "icon is-small",
+                                                if system_prompt_dropdown.is_empty() {
+                                    rsx!(
+                                        Icon { width: 24, height: 24, fill: "#6e7781", icon: BsArrowDownShort }
+                                    )
+                                } else {
+                                    rsx!(
+                                        Icon { width: 24, height: 24, fill: "#6e7781", icon: BsArrowUpShort }
+                                    )
+                                }
+                                            }
                                         }
-                                    },
-                                    span { "prompt列表" }
-                                    span { class: "icon is-small",
-                                        svg {
-                                            xmlns: "http://www.w3.org/2000/svg",
-                                            "viewBox": "0 0 24 24",
-                                            path { d: "M7,10L12,15L17,10H7Z" }
+                                    }
+
+                                    div {
+
+                                        class: "dropdown-menu",
+
+                                        id: "dropdown-menu",
+
+                                        role: "menu",
+                                        div { class: "dropdown-content",
+                                            a {
+                                                class: "dropdown-item py-0",
+                                                onclick: move |_| {
+                                                    system_prompt_dropdown.set("");
+                                                },
+                                                "关闭"
+                                            }
+                                            hr { class: "dropdown-divider" }
+                                            if system_prompts.is_empty() {
+                                rsx! {
+                                    div { class: "dropdown-item",
+                                        p {
+                                            "没有system prompts"
                                         }
                                     }
                                 }
                             }
-
-                            div { class: "dropdown-menu", id: "dropdown-menu", role: "menu",
-                                div { class: "dropdown-content",
-                                    a {
-                                        class: "dropdown-item py-0",
-                                        onclick: move |_| {
-                                            system_prompt_dropdown.set("");
-                                        },
-                                        "关闭"
-                                    }
-                                    hr { class: "dropdown-divider" }
-                                    if system_prompts.is_empty() {
-                                        rsx! {
                                             div { class: "dropdown-item",
-                                                p {
-                                                    "没有system prompts"
+                                                div { class: "columns is-multiline",
+                                                    system_prompts.iter().map(|e| {
+                                    rsx!(
+                                        div {class: "column",
+                                            span { class: "tag is-primary is-light",
+                                                onclick: move |_| {
+                                                    system_prompt_name.set(e.name.clone());
+                                                    system_prompt.set(e.content.clone());
+                                                    system_prompt_dropdown.set("");
+                                                },
+                                                "{e.name}" 
+                                            
+                                                button { class: "delete is-small",
+                                                    onclick: move |_| {
+                                                        system_prompts.with_mut(|v| {
+                                                            if let Some(p) = v.iter().position(|value| value.name.eq(&e.name)) {
+                                                                v.remove(p);
+                                                            }
+                                                        });
+                                                        save_system_prompts(&*system_prompts.current().clone());
+                                                    }
+                                                }
+                                            }
+                                        })
+                                    })
                                                 }
                                             }
                                         }
                                     }
-                                    div { class: "dropdown-item",
-                                        div { class: "columns is-multiline",
-                                            system_prompts.iter().map(|e| {
-                                            rsx!(
-                                                div {class: "column",
-                                                    span { class: "tag is-primary is-light",
-                                                        onclick: move |_| {
-                                                            system_prompt_name.set(e.name.clone());
-                                                            system_prompt.set(e.content.clone());
-                                                            system_prompt_dropdown.set("");
-                                                        },
-                                                        "{e.name}" 
-                                                    
-                                                        button { class: "delete is-small",
-                                                            onclick: move |_| {
-                                                                system_prompts.with_mut(|v| {
-                                                                    if let Some(p) = v.iter().position(|value| value.name.eq(&e.name)) {
-                                                                        v.remove(p);
-                                                                    }
-                                                                });
-                                                                save_system_prompts(&*system_prompts.current().clone());
-                                                            }
-                                                        }
-                                                    }
-                                                })
-                                            })
-                                        }
-                                    }
                                 }
                             }
                         }
                     }
+                }
+
+                div { class: "column pb-1", p { class: "has-text-grey-light", "用户prompt" } }
+            }
+
+            div { class: "columns",
+                div { class: "column pt-1",
                     p { class: "control",
                         textarea {
                             class: "textarea",
-                            rows: 2,
                             value: "{system_prompt}",
                             oninput: move |evt| {
                                 system_prompt.set(evt.value.clone());
@@ -259,10 +273,7 @@ fn app(cx: Scope) -> Element {
                         }
                     }
                 }
-            }
-            div { class: "columns",
-                div { class: "column",
-                    p { class: "has-text-grey-light", "用户prompt" }
+                div { class: "column pt-1",
                     p { class: "control {loading}",
                         textarea {
                             class: "textarea",
@@ -280,33 +291,32 @@ fn app(cx: Scope) -> Element {
                 class: "button is-primary my-1 {loading}",
                 disabled: "{request_button_disable(configuration, system_prompt, prompt)}",
                 onclick: move |_| {
-                    cx
-                        .spawn({
-                            let loading = loading.clone();
-                            loading.set("is-loading".to_string());
-                            let configuration = configuration.clone();
-                            let system_prompt = system_prompt.clone();
-                            let prompt = prompt.clone();
-                            let response = response.clone();
-                            let error_msg = error_msg.clone();
-                            async move {
-                                let result = request(
-                                        configuration.current().url_prefix.clone(),
-                                        configuration.current().secret.clone(),
-                                        system_prompt.current().to_string(),
-                                        prompt.current().to_string(),
-                                    )
-                                    .await;
-                                match result {
-                                    Ok(res) => {
-                                        error_msg.set("".to_string());
-                                        response.set(res);
-                                    }
-                                    Err(e) => error_msg.set(e.to_string()),
+                    cx.spawn({
+                        let loading = loading.clone();
+                        loading.set("is-loading".to_string());
+                        let configuration = configuration.clone();
+                        let system_prompt = system_prompt.clone();
+                        let prompt = prompt.clone();
+                        let response = response.clone();
+                        let error_msg = error_msg.clone();
+                        async move {
+                            let result = request(
+                                    configuration.current().url_prefix.clone(),
+                                    configuration.current().secret.clone(),
+                                    system_prompt.current().to_string(),
+                                    prompt.current().to_string(),
+                                )
+                                .await;
+                            match result {
+                                Ok(res) => {
+                                    error_msg.set("".to_string());
+                                    response.set(res);
                                 }
-                                loading.set("".to_string());
+                                Err(e) => error_msg.set(e.to_string()),
                             }
-                        })
+                            loading.set("".to_string());
+                        }
+                    })
                 },
                 "提交"
             }
